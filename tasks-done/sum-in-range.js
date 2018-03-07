c145eb5857926c36e6f5d321af2814051873c4f4
Marina <marina.iabluchanskaya@gmail.com>
2018-03-07 13:03:25 +0100

    update tasks

--- tasks-done/sum-in-range.js
+++ tasks-done/sum-in-range.js
@@ -1,3 +1,14 @@
+/*We'll pass you an array of two numbers. 
+Return the sum of those two numbers and all numbers between them.
+The lowest number will not always come first
+sumAll([1, 4]) should return 10.
+sumAll([4, 1]) should return 10.
+sumAll([5, 10]) should return 45.
+sumAll([10, 5]) should return 45.
+
+*/
+
+
 function sumAll(arr) {
     var sum = 0;
     var max = Math.max(...arr);
