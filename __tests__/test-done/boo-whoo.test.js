344ead1b03d7f0342fcae37a522d2ced7393206e
Marina <marina.iabluchanskaya@gmail.com>
2018-03-09 12:39:14 +0100

    test-task7

--- __tests__/test-done/boo-whoo.test.js
+++ __tests__/test-done/boo-whoo.test.js
@@ -2,8 +2,8 @@ const booWho = require("../../tasks-done/boo-who");
 
 describe("test for booWho", () => {
     test('matches', () => {
-        expect(booWho(true)).toEqual(true);
-        expect(booWho(NaN)).toEqual(false);
-        expect(booWho("true")).toEqual(false);
+        expect(booWho(true)).toBeTruthy();
+        expect(booWho(NaN)).toBeFalsy();
+        expect(booWho("true")).toBeFalsy();
     });
 });
